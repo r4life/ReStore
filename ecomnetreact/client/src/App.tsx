45fe36ee602b15e68154c171a035c824e4320e88
Sean Choi <r4life@gmail.com>
2022-09-29 23:52:32 +0100

    TS type added, CORS added

--- ecomnetreact/client/src/App.tsx
+++ ecomnetreact/client/src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import { Product } from './product';
 
 function App() {
+  const [products, setProducts] = useState<Product[]>([])
+
+  useEffect(()=>{
+    fetch('http://localhost:5000/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, [])
+
+  function addProduct(){
+    setProducts(prevState => [...prevState,
+      {
+        id: prevState.length + 101,
+        name: 'product' + (prevState.length + 1),
+        price: (prevState.length * 100) + 100,
+        brand: 'some brand',
+        description: 'some description',
+        pictureUrl: 'http://picsum.photos/200'
+      }])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Re-Store</h1>
+      <ul>
+        {products.map(product =>(
+          <li key={product.id}>{product.name} - {product.price}</li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>Add product</button>
     </div>
   );
 }
