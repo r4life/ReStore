0314f2d05aba05def5c74d02d8efd8da23595494
Sean Choi <r4life@gmail.com>
2022-09-30 02:22:36 +0100

    File and folder organisation, react component

--- ecomnetreact/client/src/app/layout/App.tsx
+++ ecomnetreact/client/src/app/layout/App.tsx
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react';
-import { Product } from './product';
+import { Product } from '../models/product';
+import Catalog from '../../features/catalog/Catalog';
+import './styles.css';
 
 function App() {
   const [products, setProducts] = useState<Product[]>([])
@@ -25,12 +27,7 @@ function App() {
   return (
     <div>
       <h1>Re-Store</h1>
-      <ul>
-        {products.map(product =>(
-          <li key={product.id}>{product.name} - {product.price}</li>
-        ))}
-      </ul>
-      <button onClick={addProduct}>Add product</button>
+      <Catalog products={products} addProduct={addProduct}/>
     </div>
   );
 }
