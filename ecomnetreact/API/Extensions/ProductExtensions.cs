68399efcaa8bea95cc5984c7def79aea14292e3b
Sean Choi <r4life@gmail.com>
2022-11-01 11:12:09 +0000

    filters for API

--- ecomnetreact/API/Extensions/ProductExtensions.cs
+++ ecomnetreact/API/Extensions/ProductExtensions.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using API.Entities;
 
@@ -18,7 +19,7 @@ namespace API.Extensions
 
 			return query;
 		}
-		
+
 		public static IQueryable<Product> Search(this IQueryable<Product> query, string searchTerm)
 		{
 			if (string.IsNullOrEmpty(searchTerm)) return query;
@@ -26,6 +27,23 @@ namespace API.Extensions
 			var lowerCaseSearchTerm = searchTerm.Trim().ToLower();
 
 			return query.Where(p => p.Name.ToLower().Contains(lowerCaseSearchTerm));
+		}
+
+		public static IQueryable<Product> Filter(this IQueryable<Product> query, string brands, string types)
+		{
+			var brandList = new List<string>();
+			var typeList = new List<string>();
+
+			if(!string.IsNullOrEmpty(brands))
+				brandList.AddRange(brands.ToLower().Split(",").ToList());
+
+			if(!string.IsNullOrEmpty(types))
+				brandList.AddRange(brands.ToLower().Split(",").ToList());
+
+			query = query.Where(p => brandList.Count == 0 || brandList.Contains(p.Brand.ToLower()));
+			query = query.Where(p => typeList.Count == 0 || typeList.Contains(p.Type.ToLower()));		
+
+			return query;			
 		}
 	}
 }
