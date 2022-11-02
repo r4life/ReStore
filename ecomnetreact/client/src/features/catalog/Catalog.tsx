6a79cf070a3ec2585a3c9603700a6c20329d449c
Sean Choi <r4life@gmail.com>
2022-11-02 13:42:39 +0000

    pagination reset when filters are used

--- ecomnetreact/client/src/features/catalog/Catalog.tsx
+++ ecomnetreact/client/src/features/catalog/Catalog.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import ProductList from "./ProductList";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
-import { fetchFilters, fetchProductsAsync, productSelectors, setProductParams } from "./catalogSlice";
+import { fetchFilters, fetchProductsAsync, productSelectors, setPageNumber, setProductParams } from "./catalogSlice";
 import { Box, Grid, Pagination, Paper, Typography } from "@mui/material";
 import ProductSearch from "./ProductSearch";
 import RadioButtonGroup from "../../app/components/RadioButtonGroup";
@@ -33,7 +33,7 @@ export default function Catalog() {
   if (status.includes('pending') || !metaData) return <LoadingComponent message='Loading products...' />
 
   return(
-    <Grid container spacing={4}>
+    <Grid container columnSpacing={4}>
       <Grid item xs={3}>
 
         <Paper sx={{mb: 2}}>
@@ -69,10 +69,10 @@ export default function Catalog() {
         <ProductList products={products} />
       </Grid>
       <Grid item xs={3} />
-      <Grid item xs={9} >
+      <Grid item xs={9} sx={{mb: 2}} >
         <AppPagination 
           metaData={metaData}
-          onPageChange={(page: number) => dispatch(setProductParams({pageNumber: page}))}
+          onPageChange={(page: number) => dispatch(setPageNumber({pageNumber: page}))}
         />
       </Grid>
     </Grid >
