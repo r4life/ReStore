4dbc9563a90d2361e4627626ee0e9cc0316d9769
Sean Choi <r4life@gmail.com>
2022-11-03 12:23:01 +0000

    Adding React Hook Form

--- ecomnetreact/client/src/features/account/Login.tsx
+++ ecomnetreact/client/src/features/account/Login.tsx
@@ -1,6 +1,4 @@
-import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -10,21 +8,14 @@ import Container from '@mui/material/Container';
 import { Link } from 'react-router-dom';
 import { Paper } from '@mui/material';
 import agent from '../../app/api/agent';
+import { FieldValues, useForm } from 'react-hook-form';
+import { LoadingButton } from '@mui/lab';
 
 export default function Login() {
-  const [values, setValues] = React.useState({
-    username: '',
-    password: ''
-  }) 
+  const {register, handleSubmit, formState: {isSubmitting}} = useForm()
 
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
-    agent.Account.login(values);
-  }
-
-  function handleInputChange(event: any) {
-    const {name, value} = event.target;
-    setValues({...values, [name]: value});
+  async function submitForm(data: FieldValues) {
+    await agent.Account.login(data);
   }
 
   return (
@@ -43,36 +34,33 @@ export default function Login() {
       <Typography component="h1" variant="h5">
         Sign in
       </Typography>
-      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+      <Box component="form" onSubmit={handleSubmit(submitForm)} noValidate sx={{ mt: 1 }}>
         <TextField
           required
           margin="normal"
           fullWidth
           id="email"
           label="Username"
-          name="username"
           autoFocus
-          onChange={handleInputChange}
-          value={values.username}
+          {...register('username')}
         />
         <TextField
           required
           margin="normal"
           fullWidth
-          name="password"
           label="Password"
           type="password"
-          onChange={handleInputChange}
-          value={values.password}
+          {...register('password')}
         />
-        <Button
+        <LoadingButton
+          loading={isSubmitting}
           type="submit"
           fullWidth
           variant="contained"
           sx={{ mt: 3, mb: 2 }}
         >
           Sign In
-        </Button>
+        </LoadingButton>
         <Grid container>
           <Grid item>
             <Link to="/register">
