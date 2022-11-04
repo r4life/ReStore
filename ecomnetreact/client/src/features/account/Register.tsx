0a714edbb903d8bf24d92bde6d9b8c859cbe06c1
Sean Choi <r4life@gmail.com>
2022-11-04 16:33:22 +0000

    Adding the registration validators

--- ecomnetreact/client/src/features/account/Register.tsx
+++ ecomnetreact/client/src/features/account/Register.tsx
@@ -11,13 +11,28 @@ import { useForm } from 'react-hook-form';
 import { LoadingButton } from '@mui/lab';
 import agent from '../../app/api/agent';
 import { useState } from 'react';
+import { toast } from 'react-toastify';
 
 export default function Register() {
-  // const history = useHistory();
+  const history = useHistory();
   const [validationErrors, setValidationErrors] = useState([])
-  const {register, handleSubmit, formState: {isSubmitting, errors, isValid}} = useForm({
+  const {register, handleSubmit, setError, formState: {isSubmitting, errors, isValid}} = useForm({
     mode: 'all'
   })
+
+  function handleApiErrors(errors: any){
+    if(errors){
+      errors.forEach((error: string) => {
+        if (error.includes('Password')){
+          setError('password', {message: error})
+        } else if ( error.includes('Email')) {
+          setError('email', {message: error})
+        } else if ( error.includes('Username')) {
+          setError('username', {message: error})
+        }
+      })
+    }
+  }
 
   return (
     <Container
@@ -37,7 +52,12 @@ export default function Register() {
       </Typography>
       <Box component="form" 
         onSubmit={handleSubmit((data) => 
-          agent.Account.register(data).catch(error => setValidationErrors(error)))} 
+          agent.Account.register(data)
+          .then(()=>{
+            toast.success('Registration successful - you can now login');
+            history.push('/login');
+          })
+          .catch(error => handleApiErrors(error)))} 
         noValidate sx={{ mt: 1 }}>
         <TextField
           required
@@ -54,7 +74,14 @@ export default function Register() {
           fullWidth
           margin="normal"
           label="Email"
-          {...register('email', {required: 'Email is required'})}
+          {...register('email', {
+            required: 'Email is required',
+            pattern: {
+              // Regex needs to be between the ticks. ( /<Your regex goes here>/ )
+              value: /^([a-zA-Z0-9_\-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/,
+              message: 'Not a valid email address'
+            }
+          })}
           error={!!errors.email}
           helperText={errors?.email?.message?.toString()}
         />
@@ -64,7 +91,13 @@ export default function Register() {
           fullWidth
           label="Password"
           type="password"
-          {...register('password', {required: 'Password is required'})}
+          {...register('password', {
+            required: 'Password is required',
+            pattern: {
+              value: /(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$/,
+              message: 'Password does not meet complexity requirements'
+            }
+          })}
           error={!!errors.password}
           helperText={errors?.password?.message?.toString()}
         />
