b9431dfe303222a8e14c544e79b8cb4417fa63a6
Sean Choi <r4life@gmail.com>
2022-11-07 14:42:33 +0000

    Refactoring identity to us an int

--- ecomnetreact/API/Entities/OrderAggregate/ShippingAddress.cs
+++ ecomnetreact/API/Entities/OrderAggregate/ShippingAddress.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Entities.OrderAggregate
 {
-    [Owned]
+  [Owned]
     public class ShippingAddress : Address
     {
         
