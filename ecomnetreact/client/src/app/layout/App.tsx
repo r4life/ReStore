b8bf185f40a945004d81c0d1cba3ab21dcacdb4d
Sean Choi <my email address used on my git repo>
2022-11-08 18:49:53 +0000

    Creating a basket slice

--- ecomnetreact/client/src/app/layout/App.tsx
+++ ecomnetreact/client/src/app/layout/App.tsx
@@ -3,12 +3,12 @@ import { Container, createTheme, CssBaseline, ThemeProvider } from '@mui/materia
 import Header from './Header';
 import { useEffect, useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import HomePage from '../../features/home/HomePage';
 import Catalog from '../../features/catalog/Catalog';
 import ProductDetails from '../../features/catalog/ProductDetails';
 import AboutPage from '../../features/about/AboutPage';
 import ContactPage from '../../features/contact/ContactPage';
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ServerError from '../errors/ServerError';
 import NotFound from '../errors/NotFound';
@@ -18,16 +18,18 @@ import agent from '../api/agent';
 import { getCookie } from '../util/util';
 import LoadingComponent from './LoadingComponent';
 import CheckoutPage from '../../features/checkout/CheckoutPage';
+import { useAppDispatch } from '../store/configureStore';
+import { setBasket } from '../../features/basket/basketSlice';
 
 function App() {
-  const {setBasket} = useStoreContext();
+  const dispatch = useAppDispatch();
   const [loading, setLoading] = useState(true);
 
   useEffect(()=>{
     const buyerId = getCookie('buyerId');
     if(buyerId){
       agent.Basket.get()
-        .then(basket => setBasket(basket))
+        .then(basket => dispatch(setBasket(basket)))
         .catch(error => console.log(error))
         .finally(()=>setLoading(false));
     } else {
