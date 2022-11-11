3d4fecf95a2816d7350bc7e5456c7f50fe28524e
Sun Choi <r4life@gmail.com>
2022-11-11 10:07:38 +0000

    Getting the filter lists from the API and storing in Redux state

--- ecomnetreact/API/Extensions/ProductExtensions.cs
+++ ecomnetreact/API/Extensions/ProductExtensions.cs
@@ -1,12 +1,10 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using API.Entities;
 
 namespace API.Extensions
 {
-    public static class ProductExtensions
+  public static class ProductExtensions
     {
         public static IQueryable<Product> Sort(this IQueryable<Product> query, string orderBy)
         {
