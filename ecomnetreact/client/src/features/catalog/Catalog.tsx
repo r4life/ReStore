708a9bf460d631b72e49b7b5fe9bb1bec5a17791
Sun Choi <r4life@gmail.com>
2022-11-14 20:23:34 +0000

    Cleaning up the loading indicator in the catalog

--- ecomnetreact/client/src/features/catalog/Catalog.tsx
+++ ecomnetreact/client/src/features/catalog/Catalog.tsx
@@ -29,7 +29,7 @@ export default function Catalog() {
     if(!filtersLoaded) dispatch(fetchFilters());
   }, [dispatch, filtersLoaded])
 
-  if (status.includes('pending') || !metaData) return <LoadingComponent message='Loading products...' />
+  if (!filtersLoaded) return <LoadingComponent message='Loading products...' />
 
   return(
     <Grid container columnSpacing={4}>
@@ -70,10 +70,11 @@ export default function Catalog() {
       </Grid>
       <Grid item xs={3} />
       <Grid item xs={9} sx={{mb:2}}>
+        {metaData && 
         <AppPagination 
           metaData={metaData}
           onPageChange={(page: number) => dispatch(setPageNumber({pageNumber: page}))}
-        />
+        />}
       </Grid>
     </Grid>
   )
