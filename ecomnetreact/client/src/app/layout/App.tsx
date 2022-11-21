113691b45d4ce0d9c63826b5f1f4703292b10b12
Sun Choi <r4life@gmail.com>
2022-11-21 05:52:14 +0000

    Cleaning up the app init code

--- ecomnetreact/client/src/app/layout/App.tsx
+++ ecomnetreact/client/src/app/layout/App.tsx
@@ -1,7 +1,7 @@
 import './styles.css';
 import { Container, createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import Header from './Header';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import HomePage from '../../features/home/HomePage';
@@ -13,12 +13,10 @@ import 'react-toastify/dist/ReactToastify.css';
 import ServerError from '../errors/ServerError';
 import NotFound from '../errors/NotFound';
 import BasketPage from '../../features/basket/BasketPage';
-import agent from '../api/agent';
-import { getCookie } from '../util/util';
 import LoadingComponent from './LoadingComponent';
 import CheckoutPage from '../../features/checkout/CheckoutPage';
 import { useAppDispatch } from '../store/configureStore';
-import { setBasket } from '../../features/basket/basketSlice';
+import { fetchBasketAsync } from '../../features/basket/basketSlice';
 import Login from '../../features/account/Login';
 import Register from '../../features/account/Register';
 import { fetchCurrentUser } from '../../features/account/accountSlice';
@@ -27,18 +25,18 @@ function App() {
   const dispatch = useAppDispatch();
   const [loading, setLoading] = useState(true);
 
-  useEffect(()=>{
-    const buyerId = getCookie('buyerId');
-    dispatch(fetchCurrentUser());
-    if(buyerId){
-      agent.Basket.get()
-        .then(basket => dispatch(setBasket(basket)))
-        .catch(error => console.log(error))
-        .finally(()=>setLoading(false));
-    } else {
-      setLoading(false);
+  const initApp = useCallback(async () => {
+    try {
+      await dispatch(fetchCurrentUser());
+      await dispatch(fetchBasketAsync());
+    } catch (error: any) {
+      console.log(error);
     }
-  }, [dispatch])
+  }, [dispatch]);
+
+  useEffect(()=>{
+    initApp().then(() => setLoading(false));
+  }, [initApp])
 
   const [darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode ? 'dark' : 'light';
--- ecomnetreact/client/src/features/basket/basketSlice.ts
+++ ecomnetreact/client/src/features/basket/basketSlice.ts
@@ -1,6 +1,7 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, isAnyOf } from "@reduxjs/toolkit";
 import agent from "../../app/api/agent";
 import { Basket } from "../../app/models/basket";
+import { getCookie } from "../../app/util/util";
 
 interface BasketState{
   basket: Basket | null;
@@ -11,6 +12,22 @@ const initialState: BasketState = {
   basket: null,
   status: 'idle'
 }
+
+export const fetchBasketAsync = createAsyncThunk<Basket>(
+  'basket/fetchBasketAsync',
+  async (_, thunkAPI) => {
+    try {
+      return await agent.Basket.get();
+    } catch(error: any) {
+      return thunkAPI.rejectWithValue({error: error.data});
+    }
+  },
+  {
+    condition: () => {
+      if (!getCookie('buyerId')) return false;
+    }
+  }
+)
 
 export const addBasketItemAsync = createAsyncThunk<Basket, {productId: number, quantity?: number}>(
   'basket/addBasketItemAsync',
@@ -70,8 +87,15 @@ export const basketSlice = createSlice({
     builder.addCase(removeBasketItemAsync.rejected, (state, action) => {
       state.status = 'idle';
       console.log(action.payload)
-    })
-
+    });
+    builder.addMatcher(isAnyOf(addBasketItemAsync.fulfilled, fetchBasketAsync.fulfilled), (state, action) => {
+      state.basket = action.payload;
+      state.status = 'idle';
+    });
+    builder.addMatcher(isAnyOf(addBasketItemAsync.rejected, fetchBasketAsync.rejected), (state, action) => {
+      state.status = 'idle';
+      console.log(action.payload);
+    });
   })
 })
 
